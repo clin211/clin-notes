ec4748940427d7e427584d56b9834dba05211d06
forest-211 <767425412lin@gmail.com>
2024-04-27 09:49:22 +0800

    deps: 添加插件

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,6 +1,19 @@
 // .vitepress/theme/index.js
-import DefaultTheme from 'vitepress/theme';
-import './custom.css';
-import './main.css';
+import DefaultTheme from "vitepress/theme";
+import vitepressNprogress from "vitepress-plugin-nprogress";
+import vitepressBackToTop from "vitepress-plugin-back-to-top";
+import "vitepress-plugin-back-to-top/dist/style.css";
+import "vitepress-plugin-nprogress/lib/css/index.css";
+import "./custom.css";
+import "./main.css";
 
-export default DefaultTheme;
+export default {
+    ...DefaultTheme,
+    enhanceApp(ctx) {
+        vitepressNprogress(ctx);
+        vitepressBackToTop({
+            // default
+            threshold: 300,
+        });
+    },
+};
